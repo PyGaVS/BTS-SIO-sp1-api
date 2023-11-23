fec750999439f3c46e7f2c777d66e25111352d05
Lylian BALL <BALL27LY@lycee-ndduroc.com>
2023-11-23 11:02:04 +0100

    tables users, user_types and agencies added + seeded

---
+++ database/seeders/AgencySeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Agency;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AgencySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $agencies = Agency::factory()
+            ->count(3)
+            ->create();
+    }
+}
